eaaade8ef0ca4ea0074209f3555d2b01ed87f093
muneeb-tariq3 <muneebtariq841@gmail.com>
2025-06-20 02:09:30 +0500

    vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Stella Vogue</a>
+      <a class="navbar-brand" href="#">Vue-Portfolio</a>
       <button
         class="navbar-toggler"
         type="button"
