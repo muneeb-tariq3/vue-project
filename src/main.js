37f256ab1ff7ef91dfa7105f5f8dd4647962fc37
Mr-Hacker <muneebtariq841@gmail.com>
2026-02-27 13:39:49 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
-import 'animate.css'
+import 'animate.css'    
+import './style.css'
+
 
 createApp(App).mount('#app')
