80706cad67f854ab44534e2cd239699773443191
Mr-Hacker <muneebtariq841@gmail.com>
2026-02-27 13:47:42 +0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <Navbar />
-  <slot /> <!-- Current page content -->
+  
+  <Home />
+  <About />
+  <Skills />
+  <Projects />
+  <Contact />
+
   <Footer />
 </template>
 
 <script setup>
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+import Skills from './components/Skills.vue'
+import Projects from './components/Projects.vue'
+import Contact from './components/Contact.vue'
 </script>
