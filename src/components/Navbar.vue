d2378a85fe532d6b7ff2e24a68a2c783ae15cfab
Mr-Hacker <muneebtariq841@gmail.com>
2026-05-30 15:50:07 +0500

    Update Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Muneeb Ur Rehman</a>
+      <a class="navbar-brand" href="#">MUNEEB UR REHMAN</a>
       <button
         class="navbar-toggler"
         type="button"
