7125072c376f8a3805515e6c051a7b3772be9376
Mr-Hacker <muneebtariq841@gmail.com>
2026-06-08 08:55:53 +0500

    Update adding BpIcons

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import 'animate.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createApp(App).mount('#app')
